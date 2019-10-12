271d22df7b53b706a7840ef9c6b93552a144bbfc
hongji3354 <hongji3354@gmail.com>
2019-10-12 17:24:11 +0900

    MyBatis와 스프링 연동  - xml 설정을 통해 MyBatis와 스프링 연동

--- ex00/src/test/java/org/zerock/persistence/DataSourceTest.java
+++ ex00/src/test/java/org/zerock/persistence/DataSourceTest.java
@@ -2,6 +2,8 @@ package org.zerock.persistence;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,11 +23,26 @@ public class DataSourceTest {
     @Setter(onMethod_ = {@Autowired})
     private DataSource dataSource;
 
+    @Setter(onMethod_ = {@Autowired})
+    private SqlSessionFactory sqlSessionFactory;
+
     @Test
     public void testConnection(){
 
         try {
             Connection con = dataSource.getConnection();
+            log.info(con);
+        }catch (Exception e){
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testMyBatis(){
+        try {
+            SqlSession session = sqlSessionFactory.openSession();
+            Connection con = session.getConnection();
+            log.info(session);
             log.info(con);
         }catch (Exception e){
             fail(e.getMessage());
