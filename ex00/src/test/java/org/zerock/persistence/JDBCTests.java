da75b18d115aebcf1662a31fdd8be786fe7cc30b
hongji3354 <hongji3354@gmail.com>
2019-10-12 16:05:57 +0900

    스프링과 Oracle Database 연동  - oracle JDBC 및 hikari Datasoure를 사용해서 xml 설정을 통해 Oracle Database와 연동

---
+++ ex00/src/test/java/org/zerock/persistence/JDBCTests.java
@@ -0,0 +1,33 @@
+package org.zerock.persistence;
+
+import lombok.extern.log4j.Log4j;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import static org.junit.Assert.*;
+
+@Log4j
+public class JDBCTests {
+
+    static {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testConnection(){
+
+        try {
+            Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","book_ex","book_ex");
+            log.info(con);
+        }catch (Exception e){
+            fail(e.getMessage());
+        }
+    }
+
+}
