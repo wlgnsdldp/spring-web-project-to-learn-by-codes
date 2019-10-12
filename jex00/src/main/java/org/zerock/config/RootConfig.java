9bd3ffa70a99c569d6c28a6fa029223b97c97ae5
hongji3354 <hongji3354@gmail.com>
2019-10-12 17:24:25 +0900

    MyBatis와 스프링 연동  - Java 설정을 통해 MyBatis와 스프링 연동

--- jex00/src/main/java/org/zerock/config/RootConfig.java
+++ jex00/src/main/java/org/zerock/config/RootConfig.java
@@ -2,6 +2,9 @@ package org.zerock.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -10,18 +13,29 @@ import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackages = {"org.zerock.sample"})
+@MapperScan(basePackages = {"org.zerock.mapper"})
 public class RootConfig {
 
     @Bean
     public DataSource dataSource(){
         HikariConfig hikariConfig = new HikariConfig();
-        hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
-        hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:orcl");
+/*        hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+        hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:orcl");*/
+        //DB log 변경으로 인한 driverClassName 및 jdbcUrl 수정
+        hikariConfig.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+        hikariConfig.setJdbcUrl("jdbc:log4jdbc:oracle:thin:@localhost:1521:orcl");
         hikariConfig.setUsername("book_ex");
         hikariConfig.setPassword("book_ex");
 
         HikariDataSource dataSource = new HikariDataSource(hikariConfig);
 
         return dataSource;
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception{
+        SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+        sqlSessionFactory.setDataSource(dataSource());
+        return sqlSessionFactory.getObject();
     }
 }
