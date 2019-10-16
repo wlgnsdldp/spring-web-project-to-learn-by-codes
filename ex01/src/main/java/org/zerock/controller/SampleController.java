bdef97d563643c0bf622b235600e429102e2851c
hongji3354 <hongji3354@gmail.com>
2019-10-16 23:13:13 +0900

    스프링 MVC Controller - Controller의 return 타입

--- ex01/src/main/java/org/zerock/controller/SampleController.java
+++ ex01/src/main/java/org/zerock/controller/SampleController.java
@@ -2,9 +2,13 @@ package org.zerock.controller;
 
 import lombok.extern.log4j.Log4j;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.zerock.domain.SampleDTO;
 import org.zerock.domain.SampleDTOList;
 import org.zerock.domain.TodoDTO;
@@ -86,11 +90,55 @@ public class SampleController {
     }
 
     @GetMapping("/ex04")
-    public String ex04(SampleDTO dto, int page){
+    public String ex04(SampleDTO dto, @ModelAttribute("page") int page){
 
         log.info("dto: " + dto);
         log.info("page: " + page);
 
         return "/sample/ex04";
+    }
+
+    @GetMapping("/ex05")
+    public void ex05(){
+        log.info("/ex05............");
+    }
+
+    @GetMapping("/ex06")
+    public @ResponseBody SampleDTO ex06(){
+        log.info("/ex06.................");
+
+        SampleDTO dto = new SampleDTO();
+        dto.setAge(10);
+        dto.setName("홍길동");
+
+        return dto;
+    }
+
+    @GetMapping("/ex07")
+    public ResponseEntity<String> ex07(){
+        log.info("/ex07...........");
+
+        String msg = "{\"name\": \"홍길동\"}";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json;charset=UTF-8");
+
+        return new ResponseEntity<>(msg, headers, HttpStatus.OK);
+    }
+
+    @GetMapping("/exUpload")
+    public void exUpload(){
+        log.info("/exUpload............");
+    }
+
+    @PostMapping("/exUploadPost")
+    public void exUpload(ArrayList<MultipartFile> files){
+
+        files.forEach(file -> {
+            log.info("-----------------------------------------");
+            log.info("name:" + file.getOriginalFilename() );
+            log.info("size:" + file.getSize());
+        });
+
     }
 }
